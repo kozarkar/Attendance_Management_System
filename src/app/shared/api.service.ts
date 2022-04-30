a136d18f955f445bede7247364f8125807856773
Kshitij Ozarkar <ksozarkar@outlook.com>
2022-04-30 15:28:59 +0530

    ..

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -10,55 +10,55 @@ export class ApiService {
   constructor(private http : HttpClient) {}
 
     postEmployee(data :any){
-      return this.http.post<any>("http://localhost:3002/posts", data)
+      return this.http.post<any>("https://my-json-server.typicode.com/kozarkar/Attendance_Management_System/posts", data)
       .pipe(map((res:any)=>{
         return res;
       }))
    }    
     getEmployee(){
-      return this.http.get<any>("http://localhost:3002/posts?_expand=department")
+      return this.http.get<any>("https://my-json-server.typicode.com/kozarkar/Attendance_Management_System/posts?_expand=department")
       .pipe(map((res:any)=>{
        return res;
     }))
  }
     updateEmployee(data :any, id : number){
-      return this.http.put<any>("http://localhost:3002/posts/"+id, data)
+      return this.http.put<any>("https://my-json-server.typicode.com/kozarkar/Attendance_Management_System/posts/"+id, data)
       .pipe(map((res:any)=>{
       return res;
       }))
    }
     deleteEmployee(id : number){
-      return this.http.delete<any>("http://localhost:3002/posts/"+id)
+      return this.http.delete<any>("https://my-json-server.typicode.com/kozarkar/Attendance_Management_System/posts/"+id)
       .pipe(map((res:any)=>{
       return res;
     }))
  }
  getAllDepartments(){
-  return this.http.get<any>("http://localhost:3002/departments")
+  return this.http.get<any>("https://my-json-server.typicode.com/kozarkar/Attendance_Management_System/departments")
   .pipe(map((res:any)=>{
    return res;
 }))
  }
  getEmployeeByDepartmentId(id:any){
-  return this.http.get<any>("http://localhost:3002/posts?departmentId=" +id)
+  return this.http.get<any>("https://my-json-server.typicode.com/kozarkar/Attendance_Management_System/posts?departmentId=" +id)
   .pipe(map((res:any)=>{
    return res;
 }))
  }
  getAllAttendance(){
-  return this.http.get<any>("http://localhost:3002/attendance")
+  return this.http.get<any>("https://my-json-server.typicode.com/kozarkar/Attendance_Management_System/attendance")
   .pipe(map((res:any)=>{
    return res;
 }))
  }
  postAttendance(data :any){
-  return this.http.post<any>("http://localhost:3002/attendance", data)
+  return this.http.post<any>("https://my-json-server.typicode.com/kozarkar/Attendance_Management_System/attendance", data)
   .pipe(map((res:any)=>{
     return res;
   }))
 } 
 getAttendanceByEmpId(id:any){
-  return this.http.get<any>("http://localhost:3002/attendance?postId="+id)
+  return this.http.get<any>("https://my-json-server.typicode.com/kozarkar/Attendance_Management_System/attendance?postId="+id)
   .pipe(map((res:any)=>{
    return res;
 }))
